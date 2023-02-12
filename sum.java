8c3d217d8b074682fbce3ed3c46ff7646ee53be7
Rishi Sachdev <rishi09.04.2001@gmail.com>
2023-02-12 15:41:59 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    public static void prints(int i,int n,int sum)
+    {
+        if(i==n)
+        {
+            sum+=i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        prints(i+1,n,sum);
+    }
+	public static void main(String[] args) {
+		prints(1,5,0);
+	}
+}
